15e19fdfa633e879b41db506f654cc0c2ada35d7
koa2019 <no-reply>
2023-03-31 19:37:53 -0700

    Restructured Admin & User classes. Now the User is the base class and Admin is derived from User class. I figured how to edit a record in binary. I used ios:ate instead of ios:app and the correct cursor. Cursor makrs the begnning of the file. I added record size and beginning of file to Admin's private members. I broke findByIndx. It works, but not correctly anymore. The problem was it wasn't finding record 0's size because of the while loop.
    Signed-off-by: koa2019 <no-reply>

--- yahtzee_v15_UserBaseAdminDerived/User.h
+++ yahtzee_v15_UserBaseAdminDerived/User.h
@@ -1,31 +1,18 @@
-/* File:   Admin.h
+/*File:   User.h
  * Author: DanYell
- * Created:March 24, 2023, 1:36 PM
+ *
+ * Created on March 28, 2023, 10:11 PM
  */
 
-#ifndef ADMIN_H
-#define ADMIN_H
-
-#include <iostream>
-#include <fstream>
+#ifndef USER_H
+#define USER_H
 #include <string> 
-using namespace std; //STD Name-space where Library is compiled
+#include<iostream>
+using namespace std;  //STD Name-space where Library is compiled
 
-class Admin {
-   
-private: // ONLY Admin has access to these member functions 
+class User {
     
-    void adminLogin();
-    bool isUsrLogin(); // Calls functions to verify login credentials
-    void updateAdmin();
-    void findByIndx(const char, int); // ReadS binary file & locate file by index
-    long findEmail(const char, string, int &); // ReadS binary file & locate file by email
-    void read1Bin(const char, long cursor=0L); // read this file binary & save
-    void wrtAdminTxt();
-    void wrtAdminBin();
-
-protected:  // Derived classes will have direct access to these member variables
-    
+protected:
     static int ttlRec; // static variable to keep track on total records in file
     int id; // unique I.D. number
     unsigned short namSiz; // length of name string
@@ -34,30 +21,36 @@ protected:  // Derived classes will have direct access to these member variables
     string email; // email
     unsigned short pwrdSiz; // length of password string
     string password; // password 
-    int hiScore; // player's highest score  
-
+    int hiScore;
+    
 public:
-
-    Admin();    // Default constructor
-    ~Admin();  // Destructor
-
+    
+    User();    
+    User(string,string,string);
+    User(int,string,string,string);
+    User(int,int,string,string,string);
+    ~User(){cout<<"\nHit User Destructor()."; }
+    
     // Mutators   
     void setID(int num) {id = num;}
     void setNamSiz(int size){  namSiz = size; }
     void setEmaiSiz(int size){ emaiSiz = size; }
     void setPwrdSiz(int size){ pwrdSiz = size; }
-    void setName(string str){  name = str; }
+    void setName(string str){  name = str; namSiz = name.size(); }
     void setEmail(string str){ email = str; }
     void setPwrd(string str){  password = str;}
     void setHiScore(int s) { hiScore = s; }
     
-     
+    // Create new User 
+    void signUp();  // Calls functions to create 1 or more profiles
+    void readInputFile();  // Reads 27 records from a file
+    
     // Validates user input before setting member variable
     bool isName();
     bool isEmail();
     bool isPwrd();
-    bool isHiScore(int score = 0); // Checks if score is higher than current hiScore
-
+    
+    
     // ! MIGHT MOVE THESE TO Protected/private !
     // Helper functions for validating member variables      
     bool isMinSize(string, int size = 2);
@@ -69,35 +62,26 @@ public:
     // Write to files
     void wrtTxt();
     void wrtBin();
-    void reWrtBin(int,long);
-    
+
     // Accessors 
     int getID() const { return id;}
     int getTtlRec() const { return ttlRec;}
-    int getHiScore() const {return hiScore;}
+     int getHiScore() const {return hiScore;}
     unsigned short getNamSiz() const {return namSiz; }
     unsigned short getEmaiSiz() const { return emaiSiz; }
     unsigned short getPwrdSiz() const {return pwrdSiz;}
     string getName() const {return name;}
     string getEmail() const {return email;}
     string getPwrd() const { return password; }
-    void print1Record() const;
+    void print1User() const;  // User friendly print out
+    void print1URec() const;
     
-    // Calls private member functions
-    bool getUsrLogin(){        
-        if(! isUsrLogin() ){ return false;} 
-        else { return true;}        
-    }
-    void getAdLogin() { adminLogin(); }
-    void adminPortal();
-       
-   
-
     // static member functions
     static void setTtlRec(int num) { ttlRec = num; }
     static void addTtlRec(); // Static member()
     static void readTtlRec();   // Read total number of records from file
     static void wrtTtlRec();    // Write the total number of records in UsrData.bin
 };
-#endif /* ADMIN_H */
+
+#endif /* USER_H */
 
